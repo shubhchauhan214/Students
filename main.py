2b76fe8ebc994689a13a21bc68229eba584465c7
shubhchauhan214 <shubhchauhan616@gmail.com>
2024-07-19 14:45:38 +0530

    Read all data

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends, HTTPException, Path
+from sqlalchemy.orm import Session
+from starlette import status
+
+from models import Students
 import models
-from database import engine
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+# Get All Data
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
+    return db.query(Students).all()
+
+
