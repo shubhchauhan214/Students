00339f526232f6e8745db88c439fcb291d7b58b6
shubhchauhan214 <shubhchauhan616@gmail.com>
2024-07-19 14:49:02 +0530

    Read data by ID

--- main.py
+++ main.py
@@ -29,3 +29,12 @@ async def read_all(db: db_dependency):
     return db.query(Students).all()
 
 
+# Read data using id
+@app.get("/student/{student_id}", status_code=status.HTTP_200_OK)
+async def student_by_id(db: db_dependency, student_id: int = Path(gt=0)):
+    student_model = db.query(Students).filter(Students.id == student_id).first()
+    if student_model is not None:
+        return student_model
+    raise HTTPException(status_code=404, detail='Student not found')
+
+
